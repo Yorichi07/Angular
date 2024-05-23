6a2a521e55896e1e5de0f77997b9916e3f3af478
Aditya07 <devganaditya@gmail.com>
2024-05-24 01:16:50 +0530

    DAY-1

---
+++ first-angular-app/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DUMMY_USERS } from './dummy-users';
+
+//typescript feature-@component
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users=DUMMY_USERS;
+  selectedUserId?:string;    //? means it can be null too
+
+  get selectedUser(){
+    return this.users.find((user)=> user.id===this.selectedUserId)!;  //by adding ! at the end we are telling TS that it will never have null value
+  }
+
+  onSelectUser(id:string){
+    this.selectedUserId=id;
+  }
+}
---
+++ first-angular-app/src/app/user/user.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, Input, Output, computed, input, output } from '@angular/core';
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// }
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  // when using signals
+  // avatar=input.required<string>();
+  // name=input.required<string>();
+
+  // imagePath=computed(()=>{ 
+  //   return 'assets/users/'+this.avatar()
+  // });
+  // select = output<string>();
+
+  @Input({ required: true }) user!: User ;
+
+  @Output() select = new EventEmitter<string>();  //now to access event object emmitted use $event argument inside event handler
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
